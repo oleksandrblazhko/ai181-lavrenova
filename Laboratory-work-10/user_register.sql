9b87b39fd31f4c2b6b54567bea781b63b5ca175c
Yelyzaveta <52915030+WeisseKatze@users.noreply.github.com>
2022-12-23 12:52:47 +0200

    Update user_register.sql

--- Laboratory-work-10/user_register.sql
+++ Laboratory-work-10/user_register.sql
@@ -5,18 +5,13 @@ CREATE OR REPLACE FUNCTION user_register(
 AS $$
 BEGIN
 	IF NOT EXISTS ( SELECT FROM probable_v2_top207 WHERE passname = v_password) THEN
-		IF password_is_correct_v2(v_password) THEN				
-			INSERT INTO users (user_name,password_hash) 
-					VALUES (v_user_name,
-						md5(v_password)); -- внесення хеш-значення паролю
-			RETURN 1;
-		ELSE
-			RAISE NOTICE 'Passname = % INCORRECT!',v_password;
-			RETURN -1;
-		END IF;
+		INSERT INTO users (user_name,password_hash) 
+			VALUES (v_user_name, md5(v_password)); -- внесення хеш-значення паролю
+		RETURN 1;
 	ELSE 
 		RAISE NOTICE 'Passname = % is bad passname',v_password;
 		RETURN -1;
 	END IF;
 END;
 $$ LANGUAGE plpgsql;
+
